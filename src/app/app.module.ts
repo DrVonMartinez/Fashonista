9781c33a61227b642f230fdb03426f8dfdf01c67
Dr Von Martinez <DrVonMartinez@users.noreply.github.com>
2020-11-12 13:25:39 -0500

    General renamings and file initializations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { HttpModule } from '@angular/http';
 import { ProductListComponent } from './product/product-list/product-list.component';
 import { ProductCreateComponent } from './product/product-create/product-create.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { AdminComponent } from './user/admin/admin.component';
+import { UserComponent } from './user/user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    ProductCreateComponent
+    ProductCreateComponent,
+    AdminComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
